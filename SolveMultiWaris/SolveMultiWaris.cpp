f66c4cccbb7220e833a4d7b65ea55d45237a1949
DimasCW <cdimas292@gmail.com>
2023-05-29 15:43:00 +0700

    membuat class budi turunan dari pekerja dan pelajar

--- SolveMultiWaris/SolveMultiWaris.cpp
+++ SolveMultiWaris/SolveMultiWaris.cpp
@@ -24,3 +24,18 @@ public:
 		cout << "pelajar dibuat\n" << endl;
 	}
 };
+
+class budi : public pekerja, public pelajar {
+public:
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur),
+		orang(pUmur)
+	{
+		cout << "budi dibuat\n" << endl;
+	}
+};
+
+int main() {
+
+}
