6f89a3b764107c765dd296e63d1611a4d25f2e67
DimasCW <cdimas292@gmail.com>
2023-05-29 15:40:32 +0700

    membuat class pelajar turuann dari class orang

--- SolveMultiWaris/SolveMultiWaris.cpp
+++ SolveMultiWaris/SolveMultiWaris.cpp
@@ -16,3 +16,11 @@ public:
 		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
 	}
 };
+
+class pelajar : virtual public orang{
+public:
+	pelajar(int pUmur) :
+		orang(pUmur) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
